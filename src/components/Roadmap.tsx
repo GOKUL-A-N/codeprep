ffa21041b2160677354985b41f9357ed8d085f83
GOKUL-A-N <gokulcricketer141103@gmail.com>
2023-10-27 19:49:18 +0530

    completed roadmap page

--- src/components/Roadmap.tsx
+++ src/components/Roadmap.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Roadmap: React.FC = () => {
   return (
     <div className="pt-10">
-      <h1 className="text-center font-bold  text-[42px]" style={{ color: "#00cfba" }}>
+      <h1 className="text-center font-bold heading1 text-[42px]">
         Roadmap
       </h1>
       <div className="design-section">
@@ -33,7 +33,7 @@ const Roadmap: React.FC = () => {
             <h2 className="paragraph font-bold bg-transparent">Linked List</h2>
           </Link>
           <Link to="../stack" className="timeline-component timeline-content">
-            <h2 className="paragraph font-bold bg-transparent">Stack</h2>
+            <h2 className="paragraph font-bold bg-transparent">Searching Algorithms</h2>
           </Link>
           <div className="timeline-middle">
             <div className="timeline-circle"></div>
@@ -45,10 +45,10 @@ const Roadmap: React.FC = () => {
             <div className="timeline-circle"></div>
           </div>
           <Link to="../queue" className=" timeline-component timeline-content">
-            <h2 className="paragraph font-bold bg-transparent">Queue</h2>
+            <h2 className="paragraph font-bold bg-transparent">Sorting Algorithms</h2>
           </Link>
           <Link to="../trees" className="timeline-component timeline-content">
-            <h2 className="paragraph font-bold bg-transparent">Trees</h2>
+            <h2 className="paragraph font-bold bg-transparent">Divide and Conquer </h2>
           </Link>
           <div className="timeline-middle">
             <div className="timeline-circle"></div>
@@ -60,10 +60,10 @@ const Roadmap: React.FC = () => {
             <div className="timeline-circle"></div>
           </div>
           <Link to="../heap" className=" timeline-component timeline-content">
-            <h2 className="paragraph font-bold bg-transparent">Heap</h2>
+            <h2 className="paragraph font-bold bg-transparent">Stack</h2>
           </Link>
           <Link to="../hashing" className="timeline-component timeline-content">
-            <h2 className="paragraph font-bold bg-transparent">Hashing</h2>
+            <h2 className="paragraph font-bold bg-transparent">Queue</h2>
           </Link>
           <div className="timeline-middle">
             <div className="timeline-circle"></div>
@@ -75,10 +75,40 @@ const Roadmap: React.FC = () => {
             <div className="timeline-circle"></div>
           </div>
           <Link to="../graph" className=" timeline-component timeline-content">
-            <h2 className="paragraph font-bold bg-transparent">Graph</h2>
+            <h2 className="paragraph font-bold bg-transparent">Tree</h2>
           </Link>
           <Link to="../matrix" className="timeline-component timeline-content">
-            <h2 className="paragraph font-bold bg-transparent">Matrix</h2>
+            <h2 className="paragraph font-bold bg-transparent">Graph</h2>
+          </Link>
+          <div className="timeline-middle">
+            <div className="timeline-circle"></div>
+          </div>
+          <div className="timeline-empty"></div>
+          <div className="timeline-empty"></div>
+
+          <div className="timeline-middle">
+            <div className="timeline-circle"></div>
+          </div>
+          <Link to="../bitmanipulation" className=" timeline-component timeline-content">
+            <h2 className="paragraph font-bold bg-transparent">Greedy Technique</h2>
+          </Link>
+          <Link to="../matrix" className="timeline-component timeline-content">
+            <h2 className="paragraph font-bold bg-transparent">Recursion</h2>
+          </Link>
+          <div className="timeline-middle">
+            <div className="timeline-circle"></div>
+          </div>
+          <div className="timeline-empty"></div>
+          <div className="timeline-empty"></div>
+
+          <div className="timeline-middle">
+            <div className="timeline-circle"></div>
+          </div>
+          <Link to="../bitmanipulation" className=" timeline-component timeline-content">
+            <h2 className="paragraph font-bold bg-transparent">Backtracking Algorithm</h2>
+          </Link>
+          <Link to="../matrix" className="timeline-component timeline-content">
+            <h2 className="paragraph font-bold bg-transparent">Dynamic Programming</h2>
           </Link>
           <div className="timeline-middle">
             <div className="timeline-circle"></div>
@@ -92,6 +122,13 @@ const Roadmap: React.FC = () => {
           <Link to="../bitmanipulation" className=" timeline-component timeline-content">
             <h2 className="paragraph font-bold bg-transparent">Bit Manipulation</h2>
           </Link>
+          <Link to="../matrix" className="timeline-component timeline-content">
+            <h2 className="paragraph font-bold bg-transparent">Math</h2>
+          </Link>
+          <div className="timeline-middle">
+            <div className="timeline-circle"></div>
+          </div>
+          <div className="timeline-empty"></div>
         </div>
       </div>
     </div>
